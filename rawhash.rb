973aa275f1d36f243c2180411474953dea29ce7f
John Towery <kbaries@gmail.com>
2018-05-03 14:19:40 -0600

    Commit to clean up

---
+++ rawhash.rb
@@ -0,0 +1,47 @@
+#<OmniAuth::AuthHash
+  credentials=  #<OmniAuth::AuthHash
+                expires=true
+                expires_at=1500678845
+                refresh_token="1/fmVsruyQLx0GQl-OKIxD03DWJfUAJZpJHqrSkvcJcUc"
+                token="ya29.GluOBCRkKOUzwF1rqhrcoaR0KbdZmEqB9RxHKDHSELwuKb4UgPxBbKBe7JYj4oplXaoz5Q425uaSWNWQOUVwRHSyOCCfMQSzWc6mbFCtWW97lXzW2tF0AgUyQtUI"
+                >
+  extra=        #<OmniAuth::AuthHash
+                id_info=  #<OmniAuth::AuthHash
+                          at_hash="TH0tlzfpS3pGUbEG967B7w"
+                          aud="213273552920-q6pvdlr59vlvassthgtevvc7i8btsdkh.apps.googleusercontent.com"
+                          azp="213273552920-q6pvdlr59vlvassthgtevvc7i8btsdkh.apps.googleusercontent.com"
+                          email="kbaries@gmail.com"
+                          email_verified=true
+                          exp=1500678844
+                          iat=1500675244
+                          iss="accounts.google.com"
+                          sub="109829373148238619026"
+                          >
+                id_token="eyJhbGciOiJSUzI1NiIsImtpZCI6IjE2NTg1ZTcxMjVlNGE0NmYwMmU2ZmEwZjBmYmY4NjNlMzRiMjg5MjIifQ.eyJhenAiOiIyMTMyNzM1NTI5MjAtcTZwdmRscjU5dmx2YXNzdGhndGV2dmM3aThidHNka2guYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMTMyNzM1NTI5MjAtcTZwdmRscjU5dmx2YXNzdGhndGV2dmM3aThidHNka2guYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDk4MjkzNzMxNDgyMzg2MTkwMjYiLCJlbWFpbCI6ImtiYXJpZXNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJUSDB0bHpmcFMzcEdVYkVHOTY3Qjd3IiwiaXNzIjoiYWNjb3VudHMuZ29vZ2xlLmNvbSIsImlhdCI6MTUwMDY3NTI0NCwiZXhwIjoxNTAwNjc4ODQ0fQ.fNZvvbm1cI133B_VKLP0YH5URMEX0MwAAkM6HVO5bHtDd74cZN40mXEKK_Nj1NvSWD0zY_KRQDifHndFn9sLEIqEQVggVc05MJL9g5s-oP-G38JMtqgyGGPqvCde1kXSX8nHtRDzakf__tn9XCkCWJ9V6RhTPW0lWNal3LKcJ_RcJXImYq19SAENFtKEWiGvP23MseLNW-FzMy6AdyjL8M_6Yr39InPeROhZ1GhAh4YU9IiXgdsU8XlnXwLCbICYcHdEg-syIwvdr8Htt8WRxt8VRxM_i5lYyqhC7y6kN0CW93Qe-jgYMwDY_iuebvTgkuc7yEuhEx03XzP6EIDmPg"
+                raw_info= #<OmniAuth::AuthHash
+                          email="kbaries@gmail.com"
+                          email_verified="true"
+                          family_name="Towery"
+                          gender="male"
+                          given_name="John"
+                          kind="plus#personOpenIdConnect"
+                          locale="en"
+                          name="John Towery"
+                          picture="https://lh6.googleusercontent.com/-ms5YXEuyN58/AAAAAAAAAAI/AAAAAAAAccQ/BgxOAvujnPQ/photo.jpg?sz=50"
+                          profile="https://plus.google.com/109829373148238619026"
+                          sub="109829373148238619026"
+                          >
+                >
+  info=         #<OmniAuth::AuthHash::InfoHash
+                email="kbaries@gmail.com"
+                first_name="John"
+                image="https://lh6.googleusercontent.com/-ms5YXEuyN58/AAAAAAAAAAI/AAAAAAAAccQ/BgxOAvujnPQ/photo.jpg"
+                last_name="Towery"
+                name="John Towery"
+                urls= #<OmniAuth::AuthHash
+                      Google="https://plus.google.com/109829373148238619026"
+                      >
+                >
+  provider=       "google_oauth2"
+  uid=            "109829373148238619026"
+>
